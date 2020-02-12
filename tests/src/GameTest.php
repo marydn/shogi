6faaa324524e9ca9adc96e492b0b47b95488f654
Mary De Nóbrega <denobrega.mary@gmail.com>
2020-02-12 17:18:59 +0100

    Added another test and refactor to fulfill Demeter's Law

--- tests/src/GameTest.php
+++ tests/src/GameTest.php
@@ -10,7 +10,7 @@ final class GameTest extends TestCase
     /** @test */
     public function it_should_create_a_new_game(): void
     {
-        $game = new Game();
+        new Game();
 
         $this->assertTrue(true);
     }
@@ -24,11 +24,9 @@ final class GameTest extends TestCase
         $game = new Game();
 
         $board = $game->board();
-        $spot = $board->spot($from);
-        $piece = $spot->piece();
+        $piece = $game->pieceFromSpot($from);
 
-        $currentPlayer = $game->currentPlayer();
-        $currentPlayer->move($piece, $to);
+        $game->currentPlayerMove($piece, $to);
 
         $oldSpot = $board->spot($from);
         $newSpot = $board->spot($to);
@@ -39,6 +37,14 @@ final class GameTest extends TestCase
 
     public function it_should_not_move_pieces_from_another_player(): void
     {
+        $from = 'A1';
+        $to = 'B2';
 
+        $game = new Game();
+        $piece = $game->pieceFromSpot($from);
+
+        $this->expectException(MoveNotAllowed::class);
+
+        $game->opposingPlayerMove($piece, $to);
     }
 }
