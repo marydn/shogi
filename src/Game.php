06e4bbd58bfc2dbdc843726f0a298daade18c024
Mary De Nóbrega <denobrega.mary@gmail.com>
2020-02-13 00:11:48 +0100

    Methods definitions for Game object

--- src/Game.php
+++ src/Game.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Shogi;
 
+use Shogi\Pieces\Pawn;
+use Shogi\Pieces\PieceInterface;
+
 final class Game
 {
     private Board $board;
@@ -12,13 +15,16 @@ final class Game
     private Player $currentPlayer;
     private array $moves;
 
-    public static function create()
+    private array $positions;
+
+    public function __construct()
     {
-        $game = new self();
-        $game->board = new Board;
-        $game->playerWhite = new Player;
-        $game->playerBlack = new Player;
-        $game->currentPlayer = $game->playerWhite;
+        $this->board = new Board;
+        $this->playerWhite = new Player;
+        $this->playerBlack = new Player;
+        $this->currentPlayer = $this->playerBlack;
+
+        $this->resetBoard();
     }
 
     public function playerWhite(): Player
@@ -34,5 +40,68 @@ final class Game
     public function currentPlayer(): Player
     {
         return $this->currentPlayer;
+    }
+
+    public function opposingPlayer(): Player
+    {
+        return $this->currentPlayer() === $this->playerWhite()
+            ? $this->playerBlack()
+            : $this->playerWhite();
+    }
+
+    public function positions(): array
+    {
+        return $this->positions;
+    }
+
+    public function pieceFromSpot(string $from): ?PieceInterface
+    {
+        [$column, $row] = str_split($from);
+
+        if (!$this->isPositionValid($column, $row)) {
+            return null;
+        }
+
+        $spot = $this->positions[$column][$row];
+
+        return $spot->piece();
+    }
+
+    public function currentPlayerMove($from, $to): void
+    {
+
+    }
+
+    public function opposingPlayerMove($from, $to): void
+    {
+
+    }
+
+    private function isPositionValid($column, $row): bool
+    {
+        return isset($this->positions[$column][$row]);
+    }
+
+    private function resetBoard(): void
+    {
+        $positions = [];
+        $columns   = range(9, 1);
+        $rows      = range('A', 'I');
+
+        foreach ($columns as $column) {
+            foreach ($rows as $row) {
+                if ($row <= 3) {
+                    $fill = Pawn::create($this->currentPlayer());
+                } else if ($row > 6) {
+                    $fill = Pawn::create($this->opposingPlayer());
+                } else {
+                    $fill = null;
+                }
+
+                $positions[$column][$row] = Spot::add($column, $row, $fill);
+            }
+        }
+
+        $this->positions = $positions;
     }
 }
