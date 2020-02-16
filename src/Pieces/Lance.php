08d03d486c60a598371ab3f55e4fce8fe54d9511
Mary De Nóbrega <denobrega.mary@gmail.com>
2020-02-16 15:02:40 +0100

    Defined behavior for Lances

--- src/Pieces/Lance.php
+++ src/Pieces/Lance.php
@@ -7,14 +7,55 @@ namespace Shogi\Pieces;
 use Shogi\Board;
 use Shogi\Spot;
 
+/**
+ * Lance's behaviour:
+ *  - Can move as many as Steps as it wants.
+ *  - Can move only in straight directions towards Opponent.
+ *  - Cannot jump over other pieces, should capture them first.
+ *  - When promoted move exactly like a Gold General.
+ */
 final class Lance extends BasePiece implements PieceInterface
 {
-    const NAME = 'L';
+    const NAME          = 'L';
+    const IS_PROMOTABLE = true;
 
-    public function canMove(Board $board, Spot $from, Spot $to): bool
+    public function isMoveAllowed(Board $board, Spot $source, Spot $target): bool
     {
-        if ($to->pieceIsWhite() === $this->isWhite()) {
+        if ($target->isTaken() && $target->pieceIsWhite() === $this->isWhite()) {
             return false;
         }
+
+        if ($target->pieceIsPromoted()) {
+            // @TODO: move like a Gold General
+        }
+
+        $x = abs($source->x() - $target->x());
+        $y = $source->y() - $target->y();
+
+        if ($this->isWhite()) {
+            $isMovingForward = $x === 0 && $y < 0;
+        } else {
+            $isMovingForward = $x === 0 && $y > 0;
+        }
+
+        $spacesInBetween = range($source->y(), $target->x());
+        $isThereAnyPieceInBetween = false;
+        foreach ($spacesInBetween as $spaceToCheck) {
+            if ($board->pieceFromSpot(sprintf('%s%s', $spaceToCheck, $source->x()))) {
+                $isThereAnyPieceInBetween = true;
+                break;
+            }
+        }
+
+        if (!$isMovingForward || $isThereAnyPieceInBetween) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public function canBePromoted(): bool
+    {
+        return self::IS_PROMOTABLE;
     }
 }
